1d22c2b93f4281327fba7811c3263d50edb7d213
Phil Helm <philip.helm@omgplc.com>
2016-12-12 13:39:13 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+let path = require('path');
+let sep = path.sep;
+
+module.exports = (parent, child) => {
+  parent = parent.split(sep);
+  child = child.split(sep);
+
+  let plength = parent.length;
+  let clength = child.length;
+
+  if (clength <= plength){
+    return false;
+  }
+  return (_.isEqual(child.slice(0, parent.length), parent));
+};
